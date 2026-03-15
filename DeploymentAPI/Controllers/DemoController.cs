2f244ac76e20638d5eb6439c756166de7b5e6a7f
Swapnali Kakade <kakadeswap@gmail.com>
2026-03-15 11:48:51 +0530

    First commit by git bash

---
+++ DeploymentAPI/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeploymentAPI.Controllers
+{
+
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        public DemoController()
+        {
+        }
+        [Route("/deployment-demo")]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("Hello World. Checking with deployment on iis");
+        }
+    }
+}
